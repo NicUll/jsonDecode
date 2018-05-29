46a7cff13256318c256e3a19a4c527b4a0617a4a
null <nic.ullman@abalon.se>
2018-05-29 13:32:28 +0200

    Made stuff

--- relreq/admin.py
+++ relreq/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from .models import Connection, JsonDictionaryEntry, DictGroup, GetType
 
 
+class JsonDictionaryEntryAdmin(admin.ModelAdmin):
+    list_display = ('jsonvalue', 'displayvalue', 'parent', 'haschildren')
+    ordering = ('parent', 'displayvalue',)
+
+class ConnectionAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'hosturl', 'restuser')
+
+class GetTypeAdmin(admin.ModelAdmin):
+    list_display = ('resourcename', 'resourcepath')
 
 
-
-admin.site.register(Connection)
+admin.site.register(Connection, ConnectionAdmin)
 admin.site.register(DictGroup)
-admin.site.register(GetType)
-admin.site.register(JsonDictionaryEntry)
+admin.site.register(GetType, GetTypeAdmin)
+admin.site.register(JsonDictionaryEntry, JsonDictionaryEntryAdmin)
