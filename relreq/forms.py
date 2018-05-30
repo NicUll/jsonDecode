88840c2657540988d9ac7aae097ffb55d57e86f6
Nic <lordtardus@gmail.com>
2018-05-30 15:52:23 +0200

    Removed requirement on data-field

--- relreq/forms.py
+++ relreq/forms.py
@@ -6,5 +6,5 @@ from relreq.models import Connection, GetType
 class RequestForm(forms.Form):
     connection = forms.ModelChoiceField(queryset=Connection.objects.all().order_by('hostname'), initial=0)
     gettype = forms.ModelChoiceField(queryset=GetType.objects.all().order_by('resourcename'), initial=0)
-    querydata = forms.CharField(max_length=50)
+    querydata = forms.CharField(max_length=50, required=False)
 
